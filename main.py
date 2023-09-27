290b8fdafc8e7ac0c698dc46aab4906eaadc9c0f
Manvir Mann <manvirm29@gmail.com>
2023-09-26 22:13:11 -0700

    Print hotp at different values

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ counter  = 0
 
 hotp = pyotp.HOTP(key)
 
+print(hotp.at(0))
+print(hotp.at(1))
+print(hotp.at(2))
+
 for _ in range(5):
     print(hotp.verify(input("Enter Code:"), counter))
     counter += 1
